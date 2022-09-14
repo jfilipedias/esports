5bd84ae037ac014b75b63137234a5c9c1bc2fe00
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-13 23:26:15 -0300

    Adds new color to bg tailwind property

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 68%);",
+        "nlw-gradient":
+          "linear-gradient(90deg, #9572FC 0%, #43E7AD 45%, #E1D55D 96%);",
       },
     },
   },
