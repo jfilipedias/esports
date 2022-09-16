35d78c0e14a2b151e1bb7603f800a218a44f8358
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-09-15 23:41:29 -0300

    Creates the base routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  const adId = request.params.id;
+
+  return response.status(201).json([]);
 });
 
 app.listen(3333);
